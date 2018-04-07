673495cca0cc4ba350750be7d275cb47d88ad55f
fathurrahman02 <37796047+fathurrahman02@users.noreply.github.com>
2018-04-06 23:47:27 -0700

    attribute dan method

--- src/praktek03/KhasiatMinuman.java
+++ src/praktek03/KhasiatMinuman.java
@@ -1,5 +1,9 @@
 package praktek03;
 
 public class KhasiatMinuman extends Minuman {
+    private String KhasiatMinuman;
     
+    void throttle(){
+        System.out.println("---Terima Kasih---");
+    }
 }
