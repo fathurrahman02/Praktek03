38435f1ef1018e0a1148fd9cc187ec127671ff10
fathurrahman02 <37796047+fathurrahman02@users.noreply.github.com>
2018-04-06 23:27:40 -0700

    Instance buat atribut dan panggil method

--- src/praktek03/MinumanAksi.java
+++ src/praktek03/MinumanAksi.java
@@ -1,5 +1,13 @@
 package praktek03;
 
 public class MinumanAksi {
-    
+    public static void main(String[] args) {
+        Minuman s = new Minuman();
+        s.merk="fanta";
+        s.rasa="melon";
+        s.bentuk="botol";
+        s.harga=5000;
+        
+        s.cetakInfo();
+    }
 }
