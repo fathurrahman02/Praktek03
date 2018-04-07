34938a17a2ab6b23368a33ae9bd6c12b1550efa0
fathurrahman02 <37796047+fathurrahman02@users.noreply.github.com>
2018-04-06 23:21:06 -0700

    buat method cetakinfo

--- src/praktek03/Minuman.java
+++ src/praktek03/Minuman.java
@@ -5,4 +5,18 @@ public class Minuman {
     String rasa;
     String bentuk;
     double harga;
+    
+    void cetakInfo(){
+
+      System.out.println("=====PRAKTEK 003=====");
+
+      System.out.println("Merk \t        : " +merk+"\n"+
+
+                "rasa         \t: " +rasa+"\n"+
+
+                "bentuk        \t: " +bentuk+"\n"+
+
+                "Harga         \t: " +harga);
+
+  }
 }
