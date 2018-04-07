aa5b4af2f56eb8ac5a605e8955bd284dbfe64b34
fathurrahman02 <37796047+fathurrahman02@users.noreply.github.com>
2018-04-06 23:35:55 -0700

    menggunakan method set

--- src/praktek03/MinumanAksi.java
+++ src/praktek03/MinumanAksi.java
@@ -3,11 +3,11 @@ package praktek03;
 public class MinumanAksi {
     public static void main(String[] args) {
         Minuman s = new Minuman();
-        s.merk="fanta";
-        s.rasa="melon";
-        s.bentuk="botol";
-        s.harga=5000;
-        
+           s.setMerk("mizone");
+           s.setRasa("leci");
+           s.setBentuk("gelas");
+           s.setHarga(3000);
+           
         s.cetakInfo();
     }
 }
